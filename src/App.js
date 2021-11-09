22ab477137f4ae5d9aad606aab463fcc61bb039e
feruza <choriyevaferuza7@gmail.com>
2021-11-09 16:46:53 +0500

    ReactLoader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+    
+   constructor(props){
+
+   super();
+   this.state={
+      name:'Abdunabi',
+      age:16,
+      job:'Programmer',
+      loading:true
+   } 
+ }
+
+//  getInfo=()=>{
+//     this.setState({name:'Shoxbaht', age:'15', job:"MERN stack Developer"})
+//  }
+
+
+ componentDidMount(){
+   fetch('https://jsonplaceholder.typicode.com/posts')
+   .then(response => response.json())
+   .then(()=> this.setState({work:'no',name:'Shoxbaht', age:'15', job:"MERN stack Developer", loading:false }))
+
+    console.log('componentDidMount')
+   //  this.getInfo();
 }
 
-export default App;
+componentDidUpdate(){
+console.log('componentDidUpdate');
+}
+  
+
+ 
+
+ render(){
+    console.log('render')
+    return(
+       <div className="card">
+  
+          {this.state.loading? 
+          <div className="load">
+
+
+<div class="loadingio-spinner-spinner-2xg9rwealxp"><div class="ldio-r07as6jfuaj">
+<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+</div></div>
+
+
+          </div>:  
+          <ul className="list"> 
+
+      <li>{this.state.name}</li>       
+      <li>{this.state.age}</li>       
+      <li>{this.state.job}</li>       
+             
+      </ul>
+ }
+       </div>
+    )
+ }
+
+
+}
